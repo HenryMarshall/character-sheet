32b82c9b819aff3892529335512698c5cbc97419
hbaughman <hbaughman741@gmail.com>
2016-12-17 22:19:38 -0500

    Add elm build step to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require("gulp")
 const sourcemaps = require("gulp-sourcemaps")
 const autoprefixer = require("gulp-autoprefixer")
 const sass = require("gulp-sass")
-const livereload = require("gulp-livereload")
+const elm = require("gulp-elm")
+const plumber = require("gulp-plumber")
+// const livereload = require("gulp-livereload")
 
 gulp.task("sass", function() {
   return gulp.src("src/sass/style.+(sass|scss)")
@@ -10,14 +12,22 @@ gulp.task("sass", function() {
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer({ browsers: ["> 1%"] }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("build/style"))
+    .pipe(gulp.dest("build"))
     // .pipe(livereload())
 })
 
-gulp.task("build", ["sass"])
+gulp.task("elm", function() {
+  return gulp.src("src/Main.elm")
+    .pipe(plumber())
+    .pipe(elm.bundle("script.js"))
+    .pipe(gulp.dest("build"))
+})
+
+gulp.task("build", ["sass", "elm"])
 
 gulp.task("watch", function() {
   gulp.watch("src/sass/**/*.+(sass|scss)", ["sass"])
+  gulp.watch("src/**/*.elm", ["elm"])
   // livereload.listen()
 })
 
