5d43da38654ea00af9034d8093ae603130099abc
hbaughman <hbaughman741@gmail.com>
2016-12-21 16:18:14 -0500

    Remove elm from gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@ const gulp = require("gulp")
 const sourcemaps = require("gulp-sourcemaps")
 const autoprefixer = require("gulp-autoprefixer")
 const sass = require("gulp-sass")
-const elm = require("gulp-elm")
 const browserSync = require("browser-sync")
 const reload = browserSync.reload
-const plumber = require("gulp-plumber")
 
 gulp.task("browser-sync", function() {
   browserSync({
@@ -23,26 +21,11 @@ gulp.task("sass", function() {
     .pipe(reload({ stream: true }))
 })
 
-gulp.task("elm", function() {
-  return gulp.src("src/Main.elm")
-    .pipe(plumber(errorHandler))
-    .pipe(elm.bundle("script.js"))
-    .pipe(gulp.dest("build"))
-    .pipe(reload({ stream: true }))
-})
-
-gulp.task("build", ["sass", "elm", "browser-sync"])
+gulp.task("build", ["sass", "browser-sync"])
 
 gulp.task("watch", function() {
   gulp.watch("src/sass/**/*.+(sass|scss)", ["sass"])
-  gulp.watch("src/**/*.elm", ["elm"])
 })
 
 gulp.task("default", ["build", "watch"])
-
-function errorHandler(err) {
-  browserSync.notify(err.message)
-  console.log(err)
-  this.emit("end")
-}
 
